ef41580e5570d1d3ca40f25c36bb1bb342bf3c75
Vaisakh Mohan <vaisakhmohan002@gmail.com>
2026-01-26 12:24:35 +0530

    Export app from server for Vercel compatibility

--- server/index.js
+++ server/index.js
@@ -48,6 +48,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
