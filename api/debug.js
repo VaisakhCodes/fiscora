0d4ce5dfb82c74f921de127f65d06a6d0482a1a2
Vaisakh Mohan <vaisakhmohan002@gmail.com>
2026-01-27 20:16:09 +0530

    Fix Vercel 404: Add build script and update debug syntax

--- api/debug.js
+++ api/debug.js
@@ -1,3 +1,3 @@
-module.exports = (req, res) => {
-    res.status(200).json({ status: 'ok', message: 'Debug endpoint working' });
-};
+export default function handler(req, res) {
+    res.status(200).json({ status: 'ok', message: 'Debug endpoint working', syntax: 'ES Module' });
+}
