d330f2498735965ef2125b9025b17ab3d904e507
Vaisakh Mohan <vaisakhmohan002@gmail.com>
2026-01-27 20:19:15 +0530

    Fix Vercel 404: Revert to CommonJS

--- api/debug.js
+++ api/debug.js
@@ -1,3 +1,3 @@
-export default function handler(req, res) {
-    res.status(200).json({ status: 'ok', message: 'Debug endpoint working', syntax: 'ES Module' });
-}
+module.exports = (req, res) => {
+    res.status(200).json({ status: 'ok', message: 'Debug endpoint working', syntax: 'CommonJS' });
+};
